d30691a02f8560f9e5b34028078aa4638907f4d4
npcipav <npcipav@gmail.com>
2016-04-23 03:40:31 +0300

    this shouldn't make page do error... :/

--- src/js/connection.js
+++ src/js/connection.js
@@ -1,7 +1,9 @@
 /**
  * connection.js
  *
- * Grabs mobile-app's IP address and manages connection to the phone.
+ * Grabs the IP address of an mobile-app and manages connection to the phone.
+ *
+ * Emits 'wsevent' events that should be handled in the view.
  *
  * In this document following terms should be treated as equal: mobile server,
  * mobile controller, phone, mobile app - and all of these mean the server running
@@ -12,56 +14,68 @@
  */
 
 (function() {
-    // Ask server if this page was requested by code.
-    var xhr = new XMLHttpRequest();
-    while (true) {
+    (new Promise(function(resolve, reject) {
+	// Ask server if this page was requested by code.
+	var xhr = new XMLHttpRequest();
 	// TODO! Edit the following reques according to protocol of the server.
-	xhr.open('GET', 'app/req', false);
-	xhr.send();
-	if (xhr.status == 200) {
-	    // This page was requested by some mobile app.
-	    break;
-	} else if (xhr.status == 204) {
-	    continue;
-	} else { /* something went wrong, amarait? */ }
-    }
-    // The followng response is a json object that may be representet in the
-    // following form:
-    //     response = {'ip': <some_ip>};
-    // where <some_ip> is a string that contains ip address.
-    response = xhr.responseText;
-    // Set the connection to the mobile app.
-    var host = "ws://" + response.ip + "/";
-    var ws = new WebSocket(host);
-
-    function onWSocketOpen() {}
-
-    function onWSocketClose(e) {
-	if (!e.wasClean) {
-	    // Retry to connect.
-	    ws = new WebSocket(host);
-	    ws.onopen = onWSocketOpen;
-	    ws.onclose = onWSocketClose;
-	    ws.onmessage = onWSocketGotMessage;
-	    return;
+	while (true) {
+	    xhr.open('GET', 'app/req', false);
+	    xhr.send();
+	    if (xhr.status == 200) {
+		// This page was requested by some mobile app.
+		break;
+	    } else if (xhr.status == 204) {
+		// Go into an infinite loop untill connection is successful.
+		continue;
+	    } else { /* something went wrong, amarait? */ }
+	    // TODO! Sleep few seconds...
 	}
-	// We handle legitimate closing in ``onmessage`` so we do nothing.
-    }
+	// The followng ``responseText`` is a json object that may be
+	// representet in the following form:
+	//     responseText = {'ip': <some_ip>};
+	// where <some_ip> is a string that contains ip address.
+	resolve(xhr.responseText);
+    })).then(function(response) {
+	// Set the connection to the mobile app.
+	var host = "ws://" + response.ip + "/";
+	var ws = new WebSocket(host);
 
-    /**
-     * Handle message from our mobile controller and emit an event notifying
-     * the view.
-     *
-     * Args:
-     *     e: An event from mobile app; contains ``time`` and ``movement``.
-     */
-    function onWSocketGotMessage(e) {
-	// Generate a new event that will be sent to event handler in the
-	// view.
-	var newEvent = new CustomEvent('wsevent', {detail: {movement: e.movement, time: e.time}});
-    }
+	function onWSocketOpen() {
+	    // After we connect the mobile server sends us data which will be
+	    // handled in ``onmessage`` so we do nothing here.
+	}
 
-    ws.onopen = onWSocketOpen;
-    ws.onclose = onWSocketClose;
-    ws.onmessage = onWSocketGotMessage;
+	function onWSocketClose(e) {
+	    if (!e.wasClean) {
+		// Retry to connect.
+		ws = new WebSocket(host);
+		ws.onopen = onWSocketOpen;
+		ws.onclose = onWSocketClose;
+		ws.onmessage = onWSocketGotMessage;
+		return;
+	    }
+	    // We handle legitimate closing in ``onmessage`` so we do nothing
+	    // here.
+	}
+
+	/**
+	 * Handle message from our mobile controller and emit an event notifying
+	 * the view.
+	 *
+	 * Args:
+	 *     e: An event from mobile app; contains ``time`` and ``movement``.
+	 */
+	function onWSocketGotMessage(e) {
+	    // Generate a new event that will be sent to event handler in the
+	    // view.
+	    var newEvent = new CustomEvent(
+		'wsevent',
+		{detail: {movement: e.movement, time: e.time}}
+	    );
+	}
+
+	ws.onopen = onWSocketOpen;
+	ws.onclose = onWSocketClose;
+	ws.onmessage = onWSocketGotMessage;
+    });
 })();
