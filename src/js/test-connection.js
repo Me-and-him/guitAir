3197760efec6eb849e90c327d5db26c1e01f8b6d
npcipav <npcipav@gmail.com>
2016-04-23 20:20:50 +0300

    edited codes

--- src/js/test-connection.js
+++ src/js/test-connection.js
@@ -5,7 +5,7 @@
 (function() {
     var newEvent = new CustomEvent(
 	'agSetupEvent',
-	{detail: {song: '12 Home.mp3', commands: [1, 2, 0, 1, 2, 0]}}
+	{detail: {song: '12 Home.mp3', commands: ['up', 'down', 'pass', 'up', 'down', 'pass']}}
     );
     document.dispatchEvent(newEvent);
 })();
