7f993e3f6f14ad0e9da24ae0e372be844b727963
npcipav <npcipav@gmail.com>
2016-04-23 06:16:57 +0300

    async ajax

--- src/js/connection.js
+++ src/js/connection.js
@@ -9,33 +9,28 @@
  * mobile controller, phone, mobile app - and all of these mean the server running
  * on the mobile phone that is a controller for this page.
  *
- * The server in this document is a server that serves as a mediator between this
- * page and the app serving as controller for it.
+ * The server in this document is a server that serves as a mediator between
+ * this page and the app serving as controller for it.
  */
 
 (function() {
-    (new Promise(function(resolve, reject) {
-	// Ask server if this page was requested by code.
-	var xhr = new XMLHttpRequest();
-	// TODO! Edit the following reques according to protocol of the server.
-	while (true) {
-	    xhr.open('GET', 'app/req', false);
-	    xhr.send();
-	    if (xhr.status == 200) {
-		// This page was requested by some mobile app.
-		break;
-	    } else if (xhr.status == 204) {
-		// Go into an infinite loop untill connection is successful.
-		continue;
-	    } else { /* something went wrong, amarait? */ }
-	    // TODO! Sleep few seconds...
-	}
-	// The followng ``responseText`` is a json object that may be
-	// representet in the following form:
-	//     responseText = {'ip': <some_ip>};
-	// where <some_ip> is a string that contains ip address.
-	resolve(xhr.responseText);
-    })).then(function(response) {
+    /**
+     * Ask server if this page was requested by code.
+     *
+     * Args:
+     *     xhr (XMLHttpRequest)
+     */
+    function requestControllerInfo(xhr) {
+	// TODO! Edit the following request according to the protocol of the
+	//     server.
+	xhr.open('GET', 'app/req', true);
+	xhr.send();
+    }
+
+    /**
+     * Set the connection to the mobile app.
+     */
+    function connectToClient(response) {
 	// Set the connection to the mobile app.
 	var host = "ws://" + response.ip + "/";
 	var ws = new WebSocket(host);
@@ -77,5 +72,20 @@
 	ws.onopen = onWSocketOpen;
 	ws.onclose = onWSocketClose;
 	ws.onmessage = onWSocketGotMessage;
-    });
+    }
+
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+	if (xhr.readyState != 4) return;
+	if (xhr.status == 200) {
+	    // The followng ``responseText`` is a json object that may be
+	    // represented in the following form:
+	    //     responseText = {'ip': <some_ip>};
+	    // where <some_ip> is a string that contains ip address.
+	    connectToClient(xhr.responseText);
+	} else if (xhr.status == 204) {
+	    // Server said that there was no phone that had requested this page.
+	    requestControllerInfo(xhr);
+	} else { /* something went wrong, amarait? */ }
+    }
 })();
