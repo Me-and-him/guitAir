379239cb4d6cfa401fd75d1e75cbf693b2fe1b1d
npcipav <npcipav@gmail.com>
2016-04-24 05:24:09 +0300

    first version of the gamelogic

--- src/js/gamelogic.js
+++ src/js/gamelogic.js
@@ -5,6 +5,9 @@
 (function() {
     //
 
+    var EPSILON = 1000;
+    var config = {};
+
     // Actions performed when current game settings recieved
     function onAgSetupEvent(event) {
 
@@ -19,13 +22,14 @@
 	    volume: 0.8,
 	});
 
-	config.currentMovements = event.detail.commands.map((currentValue, index, array) => {
-	    return {
-		index: index,
-		name: currentValue,
-		color: config.colors.neutral,
-	    }
-	});
+	// config.currentMovements = event.detail.commands.map((currentValue, index, array) => {
+	//     return {
+	// 	index: index,
+	// 	name: currentValue,
+	// 	color: config.colors.neutral,
+	//     }
+	// });
+	config.currentMovements = event.detail.commands;
 
 	// BPM, minInterval, beginning offset
 	// config.currentBpm = event.detail.bpm;
@@ -52,31 +56,47 @@
 
 	// If we're in the beginning of song
 	if (isFirst === true) {
-	    addMovementOnCanvas(config.currentMovements[0]);
-	    animateMovement(config.currentMovements[0]);
+	    //addMovementOnCanvas(config.currentMovements[0]);
+	    //animateMovement(config.currentMovements[0]);
+	    config.currentIndex = 0;
+	    var newEvent = new CustomEvent('glAddMovement', {detail: config.currentMovements[config.currentIndex]});
+	    document.dispatchEvent(newEvent);
 	    setTimeout(nextBeat, config.currentMinInterval);
 	    return;
 	}
 
 	// Insert new movement
-	var appearingMovementIndex = Math.floor((Date.now() - config.currentStartDate) / config.currentMinInterval);
-	console.log(appearingMovementIndex);
-	var appearingMovement = config.currentMovements[appearingMovementIndex];
+	//var appearingMovementIndex = Math.floor((Date.now() - config.currentStartDate) / config.currentMinInterval);
+	console.log('apearing movement index: ' + appearingMovementIndex);
+	var appearingMovement = config.currentMovements[config.currentIndex];
 
-	addMovementOnCanvas(appearingMovement);
-	animateMovement(appearingMovement);
+	var newEvent = new CustomEvent('glAddMovement', {detail: appearingMovement});
+	document.dispatchEvent(newEvent);
 	setTimeout(nextBeat, config.currentMinInterval);
 	console.log(appearingMovementIndex);
-
     }
 
-    function onAgCommandEvent(event) {}
-    
-    // document.addEventListener('agSetupEvent', onAgSetupEvent);
-    document.addEventListener('agSetupEvent', function(event) {
-	console.log('agSetupEvent: ' + JSON.stringify(event.detail));
-    });
-    document.addEventListener('agCmmandEvent', function(event) {
-	console.log('agCommandEvent' + JSON.stringify(event.detail));
-    });
+    // function onAgSetupEvent(event) {
+    // 	config.movements = event.detail.commands;
+    // }
+
+    function onAgCommandEvent(event) {
+	var time = Date.now();
+	if (Math.abs(time - event.detail.time) < EPSILON) {
+	    var newEvent = new CustomEvent('glStatus', {detail: "success"});
+	    document.addEventListener(newEvent);
+	} else {
+	    var newEvent = new CustomEvent('glStatus', {detail: "fail"});
+	    document.addEventListener(newEvent);
+	}
+    }
+
+    document.addEventListener('agSetupEvent', onAgSetupEvent);
+    // document.addEventListener('agSetupEvent', function(event) {
+    // 	console.log('agSetupEvent: ' + JSON.stringify(event.detail));
+    // });
+    Document.addEventListener('agCommandEvent', onAgCommandEvent);
+    // document.addEventListener('agCmmandEvent', function(event) {
+    // 	console.log('agCommandEvent: ' + JSON.stringify(event.detail));
+    // });
 })();
